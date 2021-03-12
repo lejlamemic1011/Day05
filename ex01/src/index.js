dec3ac7ff976027c115415a487e9ad70a9e15568
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-03-12 11:17:21 +0100

    Add files via upload

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,1 +1,15 @@
+function main() {
+   // Only change code below this line 
+    var myFristString = "I think therefore I \"am\"-- sentient." ;
+    var mySecondString = "No, you only \"think\", so you aren't." ; 
 
+   // Only change code above this line 
+
+   return {
+
+    myFristString, 
+    mySecondString
+   };
+}
+console.log(main());
+module.exports = main; 
