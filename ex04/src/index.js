08e6684e1cd63f11e97c0bbc57db1a61dc5af465
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-03-13 09:44:11 +0100

    Update index.js

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,6 +1,6 @@
 function main() {
     // Only change code below this line 
-    var myString = "This is my first string. " + "This is the second string.";
+    var myString = "This is the first string. " + "This is the second string.";
     // Only change code above this line
 
     return myString;
